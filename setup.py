bb634c9543f64cc1662011b5af2aa14fdb8f0212
yilan.zhao <yilan.zhao@emc.com>
2013-12-20 13:25:18 +0800

    add code

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/Users/zhaoy10/ENV/bin/python
+
+from setuptools import setup,find_packages
+
+
+setup(name='rake',
+	version='0.1.1',
+	author='yilan',
+	package_data = {'':['*.*']},
+	packages = find_packages()
+	)
