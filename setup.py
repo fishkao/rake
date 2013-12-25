564a51b7c289cd949a758e619d9c9bfc739abe59
yilan <zhaoyinlan@live.cn>
2013-12-25 11:28:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 #!/Users/zhaoy10/ENV/bin/python
 
+import os
 from setuptools import setup,find_packages
 
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+REQUIREMENT = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 
-setup(name='rake',
-	version='0.1.1',
-	author='yilan',
+setup(name= 'rake',
+	version='1.0',
+	author ='yilan',
+	author_email="zhaoyinlan@live.cn",
+	license="MIT",
+	keywords="Rapid automatic keywords extraction",
+	url="https://github.com/fishkao/rake",
 	package_data = {'':['*.*']},
-	packages = find_packages()
-	)
+	packages = find_packages(),
+	namespace_packages=['rake'],
+	long_description=open(README).read() + "\n\n",
+	description = ('Rapid Automatic Keywords Extraction', "Just a Practice"),
+	classifiers=[
+		"Programming Language :: Python",
+		("Topic :: Software Development :: Libraries :: Python Modules"),
+	],
+	install_requires=[x.replace("==",">=") for x in open(REQUIREMENT).read().split('\n') if x!=""],
+    )
